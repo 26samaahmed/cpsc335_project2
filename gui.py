30c7e8fe9b1a8c670c8da2ae35ed7e8c4d098379
Sama Ahmed <26samahmed@csu.fullerton.edu>
2025-03-16 23:03:15 -0700

    work more on gui

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import tkinter.ttk as ttk
 from tkinter import filedialog
 
 
@@ -29,16 +30,38 @@ root.title("Huffman Compression Tool Project 2 Team 1")
 root.geometry('700x550')
 root.configure(background='white')
 
-Label(root, text="📦 Huffman Coding Compression Tool", font=("Courier New", 18, "bold"), wraplength=500, justify="center").pack()
+Label(root, text="📦 Huffman Coding Compression Tool", font=("Courier New", 18, "bold"), wraplength=500, justify="center", bg='white').pack()
 
 selected_file_button = Button(root, text="📩 Select File", command=select_file)
 selected_file_button.pack(pady=10)
-selected_file_button.configure(background='white', font=("Courier New", 12, 'bold'), fg='black', padx=10, pady=5)
+selected_file_button.configure(background='white', font=("Courier New", 12, 'bold'), fg='black', padx=10, pady=5, bg='white')
+
+# Text box with scrollbar for huffman binary code
+Label(root, text="Huffman Codes:", font=("Courier New", 16, "bold"), wraplength=500, justify="center", background="white").pack()
+text_frame = Frame(root)
+text_frame.pack(pady=20)
+text_frame.configure(background='white', borderwidth=1)
+scrollbar = Scrollbar(text_frame)
+scrollbar.pack(side=RIGHT, fill=Y)
+text_box = Text(text_frame, height=10, width=50, yscrollcommand=scrollbar.set)
+text_box.pack(side=LEFT, fill=Y)
+scrollbar.config(command=text_box.yview, background='white')
+
+
 
 decompress_file_button = Button(root, text="🔄 Decompress File", command=decompress_file)
 decompress_file_button.pack(pady=20)
-decompress_file_button.configure(background='white', font=("Courier New", 12, 'bold'), fg='black', padx=10, pady=5)
+decompress_file_button.configure(background='white', font=("Courier New", 12, 'bold'), fg='black', padx=10, pady=5, bg='white')
 
+Label(root, text="Decoded Text:", font=("Courier New", 16, "bold"), wraplength=500, justify="center", background="white").pack()
+text_frame = Frame(root)
+text_frame.pack(pady=20)
+text_frame.configure(background='white', borderwidth=1)
+scrollbar = Scrollbar(text_frame)
+scrollbar.pack(side=RIGHT, fill=Y)
+text_box = Text(text_frame, height=5, width=50, yscrollcommand=scrollbar.set)
+text_box.pack(side=LEFT, fill=Y)
+scrollbar.config(command=text_box.yview)
 
 
 root.mainloop()
