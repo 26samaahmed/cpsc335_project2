a86e58ebed58dc0be208e245e62076e57cb35d59
michael_ngo <michael.ngo@csu.fullerton.edu>
2025-03-19 12:05:01 -0700

    Made the decoded display and changed the sizes of the gui

--- helperfunctions.py
+++ helperfunctions.py
@@ -80,6 +80,7 @@ def decompress_file():
         if temp_code in huffman_codes:
             decoded_text += huffman_codes[temp_code]
             temp_code = ""
+    print(decoded_text)
     return decoded_text
 
 def output_equals_input(decoded_text):
